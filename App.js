dc83d82150b371630c7782846aab6f55bbceba2a
Anshika <anshika@192.168.1.6>
2024-01-09 21:57:56 +0530

    1st

---
+++ App.js
@@ -0,0 +1,21 @@
+{/* <div id="parent">
+    <div id="child">
+        <h1 id="heading"></h1>
+    </div>
+</div> */}
+const heading=React.createElement("div",{id:"parent"},
+[React.createElement("div",{id:"child"},[
+    React.createElement("h1",{id:"heading"},"Hello World!"),
+    React.createElement("h2",{id:"heading1"},"Hello World1!")
+]),
+React.createElement("div",{id:"child"},[
+    React.createElement("h1",{id:"heading"},"Hello World!"),
+    React.createElement("h2",{id:"heading1"},"Hello World1!")
+])
+]
+);
+
+
+// const heading=React.createElement("h1",{id:"heading"},"Hello World!");
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
