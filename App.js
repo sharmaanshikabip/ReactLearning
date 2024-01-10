6fa98dadc5bb01ffc4fead4e2ee3cd147996961e
Anshika <anshika@192.168.1.6>
2024-01-10 21:05:48 +0530

    assignment

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 {/* <div id="parent">
     <div id="child">
         <h1 id="heading"></h1>
@@ -6,7 +8,7 @@
 const heading=React.createElement("div",{id:"parent"},
 [React.createElement("div",{id:"child"},[
     React.createElement("h1",{id:"heading"},"Hello World!"),
-    React.createElement("h2",{id:"heading1"},"Hello World1!")
+    React.createElement("h2",{id:"heading1"},"Hello!")
 ]),
 React.createElement("div",{id:"child"},[
     React.createElement("h1",{id:"heading"},"Hello World!"),
